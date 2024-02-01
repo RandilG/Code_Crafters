9b82e65293a7d7a6530ae07032c94b13b85fe7df
Randil G Dissanayake <dissanayakewrg.21@uom.lk>
2024-02-01 16:21:25 +0530

    final

--- Backend/Functions/Test/postData.js
+++ Backend/Functions/Test/postData.js
@@ -1,7 +1,20 @@
 
-module.exports = async function postData(req, res){
-    console.log(req.body.name)
-    console.log(req.body.age)
+const connection = require('./../../Services/connection')
 
-    return res.send("Data received!!")
+module.exports = async function postData(req, res){
+    const genarate = "INSERT INTO `randil_db`.`user` (`username`, `fname`, `lname`, `tpno`, `city`) VALUES (?, ?, ?, ?, ?);"
+
+    const values = [
+        req.body.username,
+        req.body.first_name,
+        req.body.last_name,
+        req.body.tp_no,
+        req.body.city
+    ]
+
+    connection.query(genarate, values, (err, data) => {
+        if(err) return res.json(err)
+        return res.json("User registered succefully")
+    })
+
 }
--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const getData = require('../Functions/Test/getData')
 const postData = require('../Functions/Test/postData')
+const deleteData = require('../Functions/Test/deleteData')
+const updateData = require('../Functions/Test/updateData')
 
 router.get('/getData', (req, res) => {
     getData(req, res)
@@ -12,5 +14,12 @@ router.post('/addData', (req, res) => {
     postData(req, res)
 })
 
+router.delete('/deleteData', (req, res) => {
+    deleteData(req, res)
+})
+
+router.put('/putData', (req, res) => {
+    updateData(req, res)
+})
 
 module.exports = router
