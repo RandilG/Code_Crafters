8872e6036fea33b485bd34dc65c488eb6ed33528
ridmikahmp21 <110852356+ridmikahmp21@users.noreply.github.com>
2024-02-29 10:50:10 +0530

    create payment

---
+++ Backend/Functions/Financial/payment.js
@@ -0,0 +1,56 @@
+const connection = require('./../../Services/connection');
+const dotenv = require('dotenv');
+const moment = require('moment');
+
+dotenv.config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async function Payment(req, res) {
+  try {
+    const customer = await stripe.customers.create({
+      email: req.body.email,
+    });
+  
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      customer: customer.id,
+      automatic_payment_methods: {
+        enabled: true,
+      }
+    });
+
+    const query = "INSERT INTO `parttime_srilanka`.`payment` (`amount`, `payment_date`, `payment_id`, `JobPoster`) VALUES (?, ?, ?, ?);";
+
+    const currentDate = moment().format('YYYY-MM-DD');
+    const values = [req.body.amount, currentDate, paymentIntent.id, req.body.JobPoster];
+
+    let response = await queryAsync(query, values);
+
+    console.log(response)
+
+    if(response.affectedRows==0){
+        return res.json("Error")
+    }else{
+        return res.json("Success")
+    }
+    
+  } catch (error) {
+    console.error('Stripe API error:', error);
+    return res.json("Internal Server Error");
+  }
+};
+
+// Helper function to wrap connection.query in a promise
+function queryAsync(query, values) {
+  return new Promise((resolve, reject) => {
+      connection.query(query, values, (err, data) => {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(data);
+          }
+      });
+  });
+}
--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -1,25 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const getData = require('../Functions/Test/getData')
-const postData = require('../Functions/Test/postData')
-const deleteData = require('../Functions/Test/deleteData')
-const updateData = require('../Functions/Test/updateData')
+const payment = require('../Functions/Financial/payment')
 
-router.get('/getData', (req, res) => {
-    getData(req, res)
-})
 
-router.post('/addData', (req, res) => {
-    postData(req, res)
-})
-
-router.delete('/deleteData', (req, res) => {
-    deleteData(req, res)
-})
-
-router.put('/putData', (req, res) => {
-    updateData(req, res)
+router.post('/payment', (req, res) => {
+    payment(req, res)
 })
 
 module.exports = router
